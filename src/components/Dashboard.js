0f4e42762f18b9c99d491d86c11c3a8e470b79a5
iris grabois <irisgrabois@gmail.com>
2023-03-07 09:58:01 +0200

    07.03

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Autocomplete from 'react-autocomplete';
+function Dashboard() {
 
-function Login() {
-  return <div>Hello, World!</div>;
+  const [value, setValue] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+  const handleSearch = (event) => {
+   fetch('https://openlibrary.org/api/books?bibkeys=ISBN:0451526538&jscmd=data&format=json')
+  .then(response => response.json())
+  .then(data => {
+    // Do something with the data
+    console.log(data)
+  });
+  };
+
+  return (
+    <div className="container">
+    <Autocomplete
+      value={value}
+      onChange={event => {
+    setValue(event.target.value);
+    handleSearch(event);}}
+      onSelect={value => setValue(value)}
+      items={suggestions}
+      getItemValue={item => item.title} // Change this depending on your data structure
+      renderItem={(item, isHighlighted) =>
+        <div key={item.id} style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
+          {item.title} 
+        </div>
+      }
+    />
+  </div>);
 }
 
-export default Login;  // <-- Make sure to export the component
+export default Dashboard;  // <-- Make sure to export the component
